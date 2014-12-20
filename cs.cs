47b0fcadaa43c04436b9b00487de056061417897
Arnaud M <hokkos@gmail.com>
2014-12-20 16:47:38 +0100

    Revert "CSharp : use the same java code"
    This reverts commit d6ed1544b3478a8acc43af0ca3886383f43962ad.

--- cs.cs
+++ cs.cs
@@ -1,76 +1,73 @@
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System;
-using System.Diagnostics;
 
-public sealed class LongestPathFinder{
-
-	int numNodes = -1;
-	readonly int[][] nodes;
-	readonly bool[] visited;
-
-	LongestPathFinder() {
-		nodes = ReadPlaces();
-		visited = new bool[numNodes];
+class longestPathFinder{
+   public node[] readPlaces(out int numNodes) {
+	string[] lines = System.IO.File.ReadAllLines("agraph");
+	numNodes = System.Convert.ToInt32(lines[0]);
+	var nodes = new node[numNodes];
+	for(int i = 0; i < numNodes; i++){
+	    node n = new node();
+	    n.neighbours = new List<route>();
+	    nodes[i]=n;
 	}
-
-	public static void Main(String[] args) {
-		var longestPathFinder = new LongestPathFinder();
-		var watch = Stopwatch.StartNew();
-		int len = longestPathFinder.GetLongestPath(0);
-		watch.Stop();
-		var elapsedMs = watch.ElapsedMilliseconds;
-		Console.WriteLine("{0} LANGUAGE CSharp {1}", len, elapsedMs);
+	for(int i = 1; i < lines.Length; i++) {
+	    string[] nums = lines[i].Split(' ');
+	    if(nums.Length < 3){
+			break;
+	    }
+	    int node = System.Convert.ToInt32(nums[0]);
+	    int neighbour = System.Convert.ToInt32(nums[1]);
+	    int cost = System.Convert.ToInt32(nums[2]);
+	    nodes[node].neighbours.Add(new route(neighbour, cost));
 	}
+	return nodes;
+    }
 
-	/**
-	 * int[node][dest|cost|...]
-	 */
-	int[][] ReadPlaces() {
-		string[] lines = System.IO.File.ReadAllLines("agraph");
-		numNodes = System.Convert.ToInt32(lines[0]);
-		var nodes = new int[numNodes][];
-		for(int i = 0; i < numNodes; i++){
-		    nodes[i]=new int[0];
-		}
-		for(int i = 1; i < lines.Length; i++) {
-		    string[] nums = lines[i].Split(' ');
-		    if(nums.Length < 3){
-				break;
-		    }
-		    int node = System.Convert.ToInt32(nums[0]);
-			int neighbour = System.Convert.ToInt32(nums[1]);
-			int cost = System.Convert.ToInt32(nums[2]);
-
-			int index = nodes[node].Length;
-			int[] replacement = new int[index + 2];
-			Array.Copy(nodes[node], replacement, index);
-			replacement[index] = neighbour;
-			replacement[index+1] = cost;
-
-			nodes[node] = replacement;
-		}
-		return nodes;
-	}
-
-	int GetLongestPath(int nodeID){
-		visited[nodeID] = true;
-		int dist, max=0;
-
-		int length = nodes[nodeID].Length;
-
-		for (int i = 0; i < length; i+=2) {
-
-			int dest = nodes[nodeID][i];
-
-			if (!visited[dest]) {
-				dist = nodes[nodeID][i + 1] + GetLongestPath(dest);
-				if (dist > max) {
-					max = dist;
-				}
+    public int getLongestPath(node[] nodes, int nodeID, bool[] visited){
+	visited[nodeID] = true;
+	int max=0;
+	var neighboursOfID = nodes[nodeID].neighbours;
+	for(int i = 0; i < neighboursOfID.Count; i++){
+		var neighbour = neighboursOfID[i];
+	    if (!visited[neighbour.dest]){
+			int dist = neighbour.cost + getLongestPath(nodes, neighbour.dest, visited);
+			if (dist > max){
+			    max = dist;
 			}
-		}
-
-		visited[nodeID] = false;
-		return max;
+	    }    
 	}
+	visited[nodeID] = false;
+	return max;
+    }
+    
+}
 
+struct route{
+	public int dest, cost;
+	public route(int dest, int cost){
+	    this.dest =dest;
+	    this.cost =cost;
+	}
+    }
+
+struct node {
+    public List<route> neighbours;
+}
+
+public class cs{
+    public static void Main(string[] args){
+	int numNodes = 0;
+	longestPathFinder p = new longestPathFinder();
+        var nodes = p.readPlaces(out numNodes);
+	bool[] visited = new bool[numNodes];
+	for(int i = 0; i < numNodes; i++){
+	    visited[i] = false;
+	}
+	long start= DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+	int result = p.getLongestPath(nodes, 0, visited);
+	long duration = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond - start;
+	System.Console.WriteLine(result + " LANGUAGE CSharp "+ duration);
+    }
 }
